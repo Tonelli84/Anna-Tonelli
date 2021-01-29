ad48afddaa478c55cc69cf512cdb54c35772f220
Tonelli84 <32524889+Tonelli84@users.noreply.github.com>
2021-01-29 15:11:50 +0100

    fixat till mobil och tagit bort en bild som stör när det är mobil sen snyggat till footer m.m

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function myFirstfunction(event) {
     event.preventDefault();
 console.log(event.srcElement.fnamn.value); 
-console.log(event.srcElement.fnamn.value); 
-console.log(event.srcElement.fnamn.value); 
-console.log(event.srcElement.fnamn.value); 
-console.log(event.srcElement.fnamn.value); 
+console.log(event.srcElement.enamn.value); 
+console.log(event.srcElement.epost.value); 
+console.log(event.srcElement.meddelande.value); 
+console.log(event.srcElement.ja.value); 
 }
