815608cd0df39ce5bb57c12333f14760e30f87d6
Tonelli84 <32524889+Tonelli84@users.noreply.github.com>
2021-02-01 10:27:37 +0100

    Lagt till ett svar när jag skickar in formulär samt centrerat med CSS

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
-function myFirstfunction(event) {
-    event.preventDefault();
-console.log(event.srcElement.fnamn.value); 
-console.log(event.srcElement.enamn.value); 
-console.log(event.srcElement.epost.value); 
-console.log(event.srcElement.meddelande.value); 
-console.log(event.srcElement.ja.value); 
+function myFirstfunction(event) 
+{
+event.preventDefault();
+
+var formData = event.srcElement;
+
+var TackHeader = document.getElementById("Tack")
+
+TackHeader.innerText = "Tack för ditt meddelande. Länge leve du!" + formData.fnamn.value + "! Vi återkommer så fort vi kan!"; 
+
+console.log(formData.fnamn.value); 
+console.log(formData.enamn.value); 
+console.log(formData.epost.value); 
+console.log(formData.meddelande.value); 
+console.log(formData.ja.value); 
 }
+
